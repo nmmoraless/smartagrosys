c5b405532857877de689788141e2afd240dc1132
Paola Jaramillo <paojami26@hotmail.com>
2024-10-14 11:55:00 -0500

    avances listas estados

---
+++ src/app/pages/services/estado.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environments } from '../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EstadoInterface } from '../interfaces/estado.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstadoService {
+
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getEstados(): Observable<EstadoInterface[]> {
+    return this.http.get<EstadoInterface[]>( this.baseUrl + `/estados`);
+  }
+
+  getEstado(idEstado: number): Observable<EstadoInterface[]> {
+    return this.http.get<EstadoInterface[]>( this.baseUrl + `/estados?id=${idEstado}`);
+  }
+
+  agregarEstado( estado: EstadoInterface ): Observable<EstadoInterface> {
+    return this.http.post<EstadoInterface>( this.baseUrl + `/estados`, estado)
+  }
+
+  actualizarEstado( estado: EstadoInterface ): Observable<EstadoInterface> {
+    return this.http.put<EstadoInterface>( this.baseUrl + `/estados?id=${estado.id}`, estado)
+  }
+
+  borrarEstado(idEstado: number): Observable<EstadoInterface> {
+    return this.http.delete<EstadoInterface>( this.baseUrl + `/estados/${idEstado}`);
+  }
+}
